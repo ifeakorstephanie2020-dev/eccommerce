7883fbbb4bb773274b48b701a65172a9b05094fa
ifeakorstephanie2020-dev <ifeakorstephanie2020@gmail.com>
2026-07-13 13:30:37 +0100

    jklhjhjkh

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-$host = 'localhost'; 
-$user = 'root'; 
-$password = ''; 
-$database = 'adaez'; 
+$host = 'sql207.infinityfree.com'; 
+$user = 'if0_42380060'; 
+$password = '35qdbGefZ8xsd5'; 
+$database = 'if0_42380060_store'; 
 
 $conn = new mysqli($host, $user, $password, $database);
